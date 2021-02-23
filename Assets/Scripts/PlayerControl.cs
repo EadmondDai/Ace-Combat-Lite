53e1d9105d2032b19dd6529d0b2cfdf54a6ead3b
Eadmond <eadmond@hotmail.com>
2021-02-23 00:56:16 -0800

    Player can move in four directions!
    Finally can move!

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    [Range(0, 100)][SerializeField] float moveSpeed;
+    [Range(0, 90)][SerializeField] float turningSpeed;
 
     // Start is called before the first frame update
     void Start()
-    {
+    { 
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalMove = Input.GetAxis("Horizontal");
-        float verticalMove = Input.GetAxis("Vertical");
+        float xMove = Input.GetAxis("Horizontal");
+        float yMove = Input.GetAxis("Vertical");
+        float deltaTime = Time.deltaTime;
+
+        Vector3 myLocalPos = transform.localPosition;
+        transform.localPosition = new Vector3(
+            myLocalPos.x + xMove * moveSpeed * deltaTime, 
+            myLocalPos.y + yMove * moveSpeed * deltaTime, 
+            myLocalPos.z);
     }
 }
