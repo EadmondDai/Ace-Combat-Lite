aca6026bddae669956248a762905cdba8ecff15e
Eadmond <eadmond@hotmail.com>
2021-02-22 22:22:45 -0800

    Back up again

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical"); 
+    }
+}
