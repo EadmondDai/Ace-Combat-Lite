60602ad5ade70b35277c718ae5a2909c48eb1c47
Eadmond <eadmond@hotmail.com>
2021-02-24 17:33:51 -0800

    Added scoreboard

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -6,9 +6,19 @@ public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] GameObject spawnParent;
+    [Range(0, 5000)] [SerializeField] int score = 500;
+
+    ScoreBoard scoreBoard;
+
+    private void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
 
     void OnParticleCollision(GameObject other)
     {
+        scoreBoard.IncreaseScore(score);
+
         GameObject newObj = Instantiate(deathVFX, transform.position, transform.rotation);
         newObj.transform.parent = spawnParent.transform;
         newObj.AddComponent<SelfDestruct>();
---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreBoard : MonoBehaviour
+{
+
+    //[SerializeField] text scoreText;
+    public int score;
+
+    public void IncreaseScore(int score)
+    {
+        this.score += score;
+    }
+}
