b0aa6fcda171dd06f64da70436da31c87e9f1d06
Eadmond <eadmond@hotmail.com>
2021-02-24 16:19:16 -0800

    The enemy explosion effect will remove itself
    Also adjusted  the enemy flying route a little.

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -5,22 +5,13 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] GameObject spawnParent;
 
     void OnParticleCollision(GameObject other)
     {
-        Instantiate(deathVFX, transform.position, transform.rotation);
+        GameObject newObj = Instantiate(deathVFX, transform.position, transform.rotation);
+        newObj.transform.parent = spawnParent.transform;
+        newObj.AddComponent<SelfDestruct>();
         Destroy(this.gameObject);
     }
 }
---
+++ Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour
+{
+    private float liveTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var particleSystem = this.GetComponent<ParticleSystem>();
+        liveTime = particleSystem.duration;
+        Invoke("suicide", liveTime);
+    }
+
+    void suicide()
+    {
+        Destroy(gameObject);
+    }
+}
