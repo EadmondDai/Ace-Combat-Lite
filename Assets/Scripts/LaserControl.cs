e49d6914662dc96e33ff4bf23ec54dd6ce762f68
Eadmond <eadmond@hotmail.com>
2021-02-26 13:17:23 -0800

    Seems to fixed the collision detection problem for the laser.

--- Assets/Scripts/LaserControl.cs
+++ Assets/Scripts/LaserControl.cs
@@ -6,12 +6,16 @@ public class LaserControl : MonoBehaviour
 {
     [SerializeField] float collisionEnableDelay = 0.2f;
     [SerializeField] float liveTime = 10;
+    [SerializeField] GameObject hitEffect;
+    [SerializeField] GameObject hitSpawnParent;
+    [SerializeField] float selfDestroyDelay = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         Invoke("enableCollision", collisionEnableDelay);
         Invoke("die", liveTime);
+        hitSpawnParent = GameObject.FindGameObjectWithTag("HitParent");
     }
 
     void die()
@@ -22,5 +26,16 @@ public class LaserControl : MonoBehaviour
     void enableCollision()
     {
         gameObject.GetComponent<CapsuleCollider>().enabled = true;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        print(name + "  hit  " + collision.gameObject.name);
+        var hitVFS = Instantiate(hitEffect, transform.position, transform.rotation);
+        hitEffect.transform.parent = hitSpawnParent.transform;
+        GetComponent<CapsuleCollider>().enabled = false;
+        GetComponent<MeshRenderer>().enabled = false;
+
+        Invoke("die", selfDestroyDelay);
     }
 }
